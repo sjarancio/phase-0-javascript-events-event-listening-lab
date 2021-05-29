dd348b6fcad8eaf2ef0ef2c4c89b41f4bdf7e8ed
Stephen Arancio <sjarancio@gmail.com>
2021-05-29 15:14:36 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+    function clickAlert() {
+      alert('Hee hee, iwas tickled');
+    }
+    
+    input.addEventListener('click', clickAlert);  
 }
